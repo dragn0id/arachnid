f58187e62a092c72b2b31cf145d1e07b32de7915
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-06-19 23:41:03 +0530

    Refactor App component structure and import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,22 @@
 /* eslint-disable no-undef */
 import "./App.css";
-import CustomGradientSelectTargetButton from "./components/customComponents/CustomGradientSelectTargetButton/CustomGradientSelectTargetButton";
-import CustomizeData from "./components/customComponents/CustomizeData/CustomizeData";
+import ArachnidHeader from "./components/customComponents/ArachnidHeader/ArachnidHeader";
 import DynamicScrapingSection from "./components/customComponents/DynamicScrapingSection/DynamicScrapingSection";
+import { DataProvider } from "./components/contexts/DataProvider";
+import CustomizeDataSection from "./components/customComponents/CustomizeDataSection/CustomizeDataSection";
 
 function App() {
   return (
     <>
-      <div className="font-alata flex flex-col w-screen text-white">
-        <header className=" p-6 space-y-6 flex flex-col justify-center items-center">
-          <h1 className="text-center font-alata text-5xl font-normal custom-gradient-text">
-            Arachnid
-          </h1>
-          <div className="flex justify-center">
-            <CustomGradientSelectTargetButton />
-          </div>
-        </header>
-        <main className="flex-1 flex flex-col p-4 space-y-6">
-          <DynamicScrapingSection />
-          <div className="bg-gray-800 p-4 rounded-md space-y-4">
-            <h2 className="text-lg font-semibold">CUSTOMISE DATASET</h2>
-            <CustomizeData />
-            <div className="flex items-center space-x-4">
-              <div className="flex items-center space-x-2">
-                <label htmlFor="outputFormat" className="text-sm">
-                  Output Format
-                </label>
-                <div className="flex items-center bg-gray-700 rounded-md">
-                  <input
-                    type="radio"
-                    name="outputFormat"
-                    id="json"
-                    className="form-radio"
-                    defaultChecked
-                  />
-                  <label htmlFor="json" className="text-xs px-2 py-1">
-                    JSON
-                  </label>
-                  <input
-                    type="radio"
-                    name="outputFormat"
-                    id="csv"
-                    className="form-radio"
-                  />
-                  <label htmlFor="csv" className="text-xs px-2 py-1">
-                    CSV
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
+      <DataProvider>
+        <div className="font-poppins flex flex-col w-screen text-white">
+          <ArachnidHeader />
+          <main className="flex-1 flex flex-col space-y-6">
+            <DynamicScrapingSection />
+            <CustomizeDataSection />
+          </main>
+        </div>
+      </DataProvider>
     </>
   );
 }
