ac2611b6d06eb8e1458c298da053cfa26dc5b0c1
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-07-14 20:12:58 +0530

    Implement function to detect scrapable data
    Co-authored-by: Hrishik <116365046+fringewidth@users.noreply.github.com>

--- src/components/customComponents/utils/ScrapeData.js
+++ src/components/customComponents/utils/ScrapeData.js
@@ -1,7 +1,45 @@
 export default function scrapeData() {
+  function getElementsOfScrapableData(target) {
+    /**
+     * This algorithm identifies all scrapable data associated with a given target element (where a mouse click occurred).
+     * We start with all elements that have the same tag and class as the target element.
+     * Recursively call the function with the parents of the candidate elements.
+     * Continue this process until the target's ancestor shares the same parent with at least another candidate element.
+     * The elements that share the same parent are identified as the scrapable data, all other elements are discarded.
+     **/
+    function helper(candidateSet) {
+      let parentSet = new Set();
+      for (let candidate of candidateSet) {
+        // if a common parent exists and it contains the target
+        if (
+          parentSet.has(candidate.parentNode) &&
+          candidate.parentNode.contains(target)
+        ) {
+          console.log(
+            "candidate.parentNode.querySelectorAll(candidate.className)",
+            candidate.parentNode.querySelectorAll("." + candidate.className)
+          );
+          // return all candidates that is the ancestor of the same class of the target element
+          return candidate.parentNode.querySelectorAll(
+            `.${candidate.className}`
+          );
+        } else parentSet.add(candidate.parentNode);
+      }
+      return helper(parentSet);
+    }
+    let className = target.className;
+    let tagName = target.tagName;
+    let candidateElements = document.querySelectorAll(
+      className ? `${tagName}.${className}` : tagName
+    );
+    return helper(new Set(candidateElements));
+  }
+
   function isBlockElement(element) {
     return window.getComputedStyle(element).display === "block";
   }
+
+  let data = {};
 
   // gets the direct text under an element, and the text of it's inline children
   function getInsideText(element) {
@@ -20,9 +58,26 @@ export default function scrapeData() {
   }
 
   const pageHandleClick = (event) => {
+    //TODO: this is a temporary fix of this annoying bug. Proper bug fix needed
+    if (
+      getInsideText(event.target) === "" &&
+      event.target.src === undefined &&
+      event.target.href === undefined
+    ) {
+      event.target.removeEventListener("click", pageHandleClick);
+    }
     event.preventDefault();
     event.stopPropagation();
-    alert(getInsideText(event.target) || event.target.href || event.target.src);
+
+    let elementArray = getElementsOfScrapableData(event.target);
+
+    elementArray.forEach((element) => {
+      element.style.border = "2px solid #0f0";
+    });
+
+    console.log("elementArray", elementArray);
+
+    // data = getDataFromIdArray(idArray);
 
     document.querySelectorAll("*").forEach((el) => {
       el.style.outline = "";
@@ -35,6 +90,14 @@ export default function scrapeData() {
   };
 
   const pageHandleMouseOut = (event) => {
+    //TODO: this is a temporary fix of this annoying bug. Proper bug fix needed
+    if (
+      getInsideText(event.target) === "" &&
+      event.target.src === undefined &&
+      event.target.href === undefined
+    ) {
+      event.target.removeEventListener("mouseout", pageHandleMouseOut);
+    }
     event.stopPropagation();
     event.target.style.border = "";
     // document
@@ -45,15 +108,25 @@ export default function scrapeData() {
   };
 
   const pageHandleMouseOver = (event) => {
+    //TODO: this is a temporary fix of this annoying bug. Proper bug fix needed
+    if (
+      getInsideText(event.target) === "" &&
+      event.target.src === undefined &&
+      event.target.href === undefined
+    ) {
+      event.target.removeEventListener("mouseover", pageHandleMouseOver);
+    }
     event.stopPropagation();
     event.target.style.border = "2px solid #f00";
+
+    // elementArray = getElementsOfScrapableData(event.target);
     // let className = event.target.className;
     // let tagName = event.target.tagName;
     // document
     //   .querySelectorAll(className ? `${tagName}.${className}` : tagName)
     //   .forEach((el) => {
     //     el.style.border = "1px solid #0f0";
-    //   });
+    // });
   };
 
   document.querySelectorAll("*").forEach((el) => {
@@ -70,4 +143,5 @@ export default function scrapeData() {
       el.addEventListener("click", pageHandleClick);
     }
   });
+  // return data;
 }
