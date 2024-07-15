8a006f445e7575ec85b9b4189312b1d22f65199a
JKrishnaKaarthik <kaarthikj511@gmail.com>
2024-07-15 22:36:01 +0530

    refactor: Update AnonymizationFunctions to exclude keys with string values or null
    Co-authored-by: Hrishik <116365046+fringewidth@users.noreply.github.com>
    Co-authored-by: Dhyaan Kotian <Dhyaan1@users.noreply.github.com>
    Co-authored-by: Dharmisht <dragn0id@users.noreply.github.com>

--- src/components/customComponents/utils/AnonymizationFunctions.js
+++ src/components/customComponents/utils/AnonymizationFunctions.js
@@ -1,16 +1,17 @@
 function filterNumericData(data) {
+  console.log("filterNumericData");
   if (data.length === 0) return [];
 
   // Get keys from the first object
   const keys = Object.keys(data[0]);
 
-  // Determine which keys have string values in the first object
-  const stringKeys = keys.filter((key) => typeof data[0][key] === "string");
+  // Determine which keys have string values or are null in the first object
+  const excludedKeys = keys.filter((key) => typeof data[0][key] === "string" || data[0][key] === null);
 
-  // Return a new array of objects, excluding the keys with string values
+  // Return a new array of objects, excluding the keys with string values or null
   return data.map((item) => {
     const newItem = { ...item };
-    stringKeys.forEach((key) => delete newItem[key]);
+    excludedKeys.forEach((key) => delete newItem[key]);
     return newItem;
   });
 }
@@ -18,18 +19,35 @@ function filterNumericData(data) {
 function perturbNumericData(data) {
   // First, filter out non-numeric data
   const filteredData = filterNumericData(data);
-
+  console.log("filteredData", filteredData);
+  console.log("perturbNumericData");
   // Then, perturb the numeric data, ignoring the 'id' column
   return filteredData.map((item) => {
     const perturbedItem = { ...item };
     Object.keys(perturbedItem).forEach((key) => {
       if (key !== "id" && typeof perturbedItem[key] === "number") {
-        // Perturb the numeric value, for example, by adding a random value between -5 and 5
-        perturbedItem[key] += (Math.random() - 0.5) * 10;
+        const order = Math.pow(
+          10,
+          Math.floor(Math.log10(Math.abs(perturbedItem[key])))
+        );
+        perturbedItem[key] += (Math.random() * 2 - 1) * order;
       }
     });
     return perturbedItem;
   });
 }
 
-export { filterNumericData, perturbNumericData };
+function multiplicativeNoise(data) {
+  const filteredData = filterNumericData(data);
+  return filteredData.map((item) => {
+    const perturbedItem = { ...item };
+    Object.keys(perturbedItem).forEach((key) => {
+      if (key !== "id" && typeof perturbedItem[key] === "number") {
+        perturbedItem[key] *= Math.random() * 2 - 1;
+      }
+    });
+    return perturbedItem;
+  });
+}
+
+export { filterNumericData, perturbNumericData, multiplicativeNoise };
