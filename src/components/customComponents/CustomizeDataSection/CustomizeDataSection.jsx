b86e83fe7e24e48cd8daaa909398e7a08fb7bbcf
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2024-07-17 14:19:43 +0530

    Add selectively filtering option to CustomizeDataSection to show up before Dynamic table

--- src/components/customComponents/CustomizeDataSection/CustomizeDataSection.jsx
+++ src/components/customComponents/CustomizeDataSection/CustomizeDataSection.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import DynamicTable from "./DynamicTable/DynamicTable";
 import OutputFormatSelectionSection from "./OutputFormatSelectionSection/OutputFormatSelectionSection";
+import SelectiveFilterScrapedDataDisplay from "./SelectiveFilterScrapedDataDisplay/SelectiveFilterScrapedDataDisplay";
 
 export default function CustomizeDataSection() {
+  const [isSelectivelyFiltering, setIsSelectivelyFiltering] = useState(true);
+
   return (
     <>
       <div>
         <h2 className="text-white text-base font-semibold border-y border-t-white border-b-slate-500 p-2 w-full">
           CUSTOMISE DATASET
         </h2>
-        <DynamicTable />
+        {isSelectivelyFiltering ? (
+          <SelectiveFilterScrapedDataDisplay
+            setIsSelectivelyFiltering={setIsSelectivelyFiltering}
+          />
+        ) : (
+          <DynamicTable />
+        )}
       </div>
       <OutputFormatSelectionSection />
     </>
