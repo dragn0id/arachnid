1beaacf8045954890d1a6123ea6ddd4dec733296
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-07-20 14:16:55 +0530

    Fix modal not visible error

--- public/content.js
+++ public/content.js
@@ -1,7 +1,18 @@
 const arachnidCreateModal = () => {
+  // check if modal is created
+  if (document.getElementById("arachnidModal")) return;
   const modal = document.createElement("iframe");
+  modal.id = "arachnidModal";
   modal.src = chrome.runtime.getURL("index.html");
-  console.log("created");
+  modal.style.cssText = `
+      position: fixed;
+      top: 0;
+      right: 0;
+      height: 100%;
+      box-shadow: -2px 0 5px rgba(0, 0, 0, 0.3);
+      z-index: 10000;
+      width: 400px;
+    `;
   document.body.appendChild(modal);
 };
 
